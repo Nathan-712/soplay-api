d7ed3357b2a5df85f6cc22eb69709d748f77cf03
Mega.edunav.net--Nathh <yusant0304@mega.sch.id>
2026-06-25 21:30:25 +0700

    Update catalog.js

--- api/catalog.js
+++ api/catalog.js
@@ -8,7 +8,7 @@ module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=300');
+  res.setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=300'); 
 
   if (req.method === 'OPTIONS') return res.status(204).end();
 
